0ae5f9441fd5467594b798106be810de43e53b4b
Lori Garzio <lgarzio@marine.rutgers.edu>
2022-02-05 14:08:02 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='glider-plotting-qc',
+    packages=find_packages(),
+    url='https://github.com/gliderqctest/glider-plotting-qc',
+    author='Laura Nazzaro, Lori Garzio',
+    author_email='nazzaro@marine.rutgers.edu, lgarzio@marine.rutgers.edu',
+    description='Test repository for scripts for plotting glider data QC flags.'
+)

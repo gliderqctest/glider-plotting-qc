08cad8028fdc8c7e5e91627bb5b735812a128c9e
Lori Garzio <lgarzio@marine.rutgers.edu>
2022-02-08 15:27:54 -0500

    add plotting xsections

--- functions/common.py
+++ functions/common.py
@@ -5,6 +5,17 @@ Author: Lori Garzio on 1/26/2022
 Last modified: 1/26/2022
 """
 from erddapy import ERDDAP
+import cmocean as cmo
+
+
+def colormaps():
+    cmaps = dict(conductivity='jet',
+                 temperature=cmo.cm.thermal,
+                 salinity=cmo.cm.haline,
+                 density=cmo.cm.dense,
+                 oxygen_concentration=cmo.cm.oxy,
+                 oxygen_saturation='viridis')
+    return cmaps
 
 
 def define_instrument_variables(instrument):
